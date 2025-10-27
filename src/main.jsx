4519ff010d3d6c2981292c2049b916323aa6e749
Nihalhussainy <nihal.sd007@gmail.com>
2025-10-27 15:17:44 +0530

    vercel anytcs

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom'; 
 import ReactGA from 'react-ga4'; // <-- Add this import
 ReactGA.initialize('G-5G7LNH0S9J'); 
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
